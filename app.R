5078b0f1984c3967d80cd1088f19aa6f2dd4a309
MrT-Stephens <92452307+MrT-Stephens@users.noreply.github.com>
2022-11-07 18:55:21 +0000

    R-Shiny-Updated

--- app.R
+++ app.R
@@ -1,16 +1,9 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 # Including the library needed.
 library(shiny)
 library(ggplot2)
 library(DT)
+library(shinythemes)
+library(bslib)
 
 # Code for reading the .csv file and the data in it.
 mobility <- read.csv("movement_data.csv", sep = ';')
@@ -18,39 +11,50 @@ mobility$Date <- as.Date(mobility$Date)
 mobility$Province <- as.factor(mobility$Province)
 
 # Ui code.
-ui <- fluidPage(tabsetPanel(
-  tabPanel("Main Page!",
+ui <- fluidPage(theme = bs_theme(version = 4, bootswatch = "flatly"),
+  navbarPage(title = h4(strong("Shiny Web Dashboard")),
+  tabPanel(h4(strong("Main Page!")),
            
            # Application title.
-           titlePanel("COVID-19 Data!"),
+           titlePanel(h1(strong("COVID-19 Data!"))),
+           h5(strong("By: Tom Stephens (30048598)")),
            sidebarLayout(
              sidebarPanel(
-               selectInput(inputId = "dv", label = "Category",
+               h6(strong(radioButtons(inputId = "dv", label = h4(strong("Category:")),
                            choices = c("Retail_Recreation", "Grocery_Pharmarcy", "Parks", "Transit_Stations", "Workplaces", "Residential"),
-                           selected = "Grocery_Pharmarcy"),
-               selectInput(inputId = "provinces", "Province(s)",
+                           selected = "Grocery_Pharmarcy"))),
+               h6(strong(checkboxGroupInput(inputId = "provinces", h4(strong("Province(s):")),
                            choices = levels(mobility$Province),
-                           multiple = TRUE,
-                           selected = c("Ctesiphon", "Petra", "Pompeii")),
-               dateRangeInput(inputId = "date", label = "Date Range",
+                           selected = c("Ctesiphon", "Petra", "Pompeii")))),
+               h6(strong(dateRangeInput(inputId = "date", label = h4(strong("Date Range:")),
                               start = min(mobility$Date),
-                              end = max(mobility$Date)),
-               downloadButton(outputId = "download_data", label = "Download")
+                              end = max(mobility$Date)))),
+               downloadButton(outputId = "download_data", label = h5(strong("Download"))),
              ),
              mainPanel(
-               plotOutput(outputId = "plot0"),
-               em("Positive and negative percentages indicate an increase and decrease the aseline period(median value between January 3 and Febuary 6, 2020) respectively."),
-               DT::dataTableOutput(outputId = "table"),
+               tabsetPanel(
+                 tabPanel(h6(strong("Plot Scatter Graph!")), plotOutput(outputId = "plot0")),
+                 tabPanel(h6(strong("Plot Line Graph!")), plotOutput(outputId = "plot1")),
+                 tabPanel(h6(strong("Plot Scatter with Line Graph!")), plotOutput(outputId = "plot2")),
+               ),
+               p(h6("More infomation about the data set used is avalible at: ", 
+                 tags$a(href="https://support.google.com/covid19-mobility/answer/9824897?hl=en&ref_topic=9822927", strong("Click Here!")))),
+               p(h6("All of the code is avalible at GitHub: ", 
+                    tags$a(href="https://github.com/MrT-Stephens/R-Shiny-WebApp", strong("Click Here!")))),
+               img(src = "rshinylogo.png", height = 150, width = 450, align = "right"),
              )
            )
            ),
-  tabPanel("Extra Graphs!",
+  tabPanel(h4(strong("Table of contents!")),
            
            # Application title.
-           titlePanel("COVID-19 Data - Extra Graphs!"),
-           plotOutput(outputId = "plot1"),
-           plotOutput(outputId = "plot2"),
-           ),
+           titlePanel(h1(strong("COVID-19 Data - Table of contents!"))),
+           h5(strong("By: Tom Stephens (30048598)")),
+           br(),
+           h6(DT::dataTableOutput(outputId = "table")),
+           h6("Positive and negative percentages indicate an increase and decrease the aseline period(median value between January 3 and Febuary 6, 2020) respectively."),
+           img(src = "rshinylogo.png", height = 150, width = 450, align = "right"),
+  ),
 )
 )
 
@@ -67,11 +71,11 @@ server <- function(input, output) {
   })
   output$plot1 <- renderPlot({
     ggplot(filtered_data(),
-           aes_string(x="Date",y=input$dv, color = "Province")) + geom_point(alpha = 0.5) + geom_line() + ylab("%change from baseline")
+           aes_string(x="Date",y=input$dv, color = "Province")) + geom_line() + ylab("%change from baseline")
   })
   output$plot2 <- renderPlot({
     ggplot(filtered_data(),
-           aes_string(x="Date",y=input$dv, color = "Province")) + geom_line() + ylab("%change from baseline")
+           aes_string(x="Date",y=input$dv, color = "Province")) + geom_point(alpha = 0.5) + geom_line() + ylab("%change from baseline")
   })
   output$table <- DT::renderDataTable({
     filtered_data()
